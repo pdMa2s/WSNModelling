1753fe80aeb309eec553a17be51180034a0a023f
iamantunes <antunes.a@ua.pt>
2020-01-13 17:15:32 +0000

    add filtered data csv. add filters and main in data_processor

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def split_data(y, x, test_size) -> (np.array, np.array, np.array, np.array):
@@ -10,7 +11,7 @@ def split_features_and_target(_data: np.ndarray, target_indexes: [int]) -> (np.a
     return np.array([_data[:, idx] for idx in target_indexes]).transpose(), np.delete(_data, target_indexes, axis=1)
 
 
-def calculate_column_variance(_data: np.array, /, index_col1, index_col2):
+def calculate_column_variance(_data: np.array, index_col1, index_col2):
     return _data[:, index_col1] - _data[:, index_col2]
 
 
@@ -27,3 +28,64 @@ def process_differential_column(_data: np.array, right_columns: list, left_colum
         differential_data[:, right] = variance_column
     differential_data = np.delete(differential_data, left_columns, axis=1)
     return differential_data
+
+
+def calc_valves_from_tanks(dataframe):
+    valves = dataframe.copy()
+    valves['dH'] = valves['Res_Espinheira'] - valves['Res_Espinheira'].shift().fillna(0)
+    valves['Val_Espinheira'] = valves['dH'].where(valves['dH'] > 0, 0)
+    valves['Val_Espinheira'] = valves['Val_Espinheira'].where(valves['Val_Espinheira'] <= 0, 1)
+    valves.drop(['dH'], axis=1, inplace=True)
+    return valves
+
+
+def calc_level_variation(dataframe):
+    variations = dataframe.copy()
+    variations['Time'] = pd.to_datetime(variations['Time'])
+    variations.set_index('Time', drop=False, inplace=True)
+    variations['deltaT'] = (variations['Time'] - variations['Time'].shift())
+    for col in dataframe.columns:
+        if col.startswith('Res'):
+            variations[col] = (dataframe[col] - dataframe[col].shift()) / (variations['deltaT'] / pd.Timedelta('1H'))
+    variations.drop(['deltaT'], inplace=True, axis=1)
+    variations.dropna(inplace=True)
+    return variations
+
+
+def filter_tanks_data(dataframe):
+    dataframe['Time'] = pd.to_datetime(dataframe['Time'])
+    dataframe.set_index('Time', drop=False, inplace=True)
+    for col in dataframe.columns:
+        if col != 'Time':
+            dataframe[col].interpolate('time', inplace=True)
+    return dataframe
+
+
+def pumps_to_bool(dataframe):
+    for col in dataframe.columns:
+        if col.startswith('P_'):
+            dataframe[col] = dataframe[col].mask(dataframe[col] > 0.5, 1)
+            dataframe[col] = dataframe[col].mask(dataframe[col] <= 0.5, 0)
+    return dataframe
+
+
+def demands_to_volume(dataframe):
+    dataframe['deltaT'] = dataframe['Time'].diff()
+
+    for col in dataframe.columns:
+        if col.startswith('PE_'):
+            dataframe[col] = dataframe[col] / (dataframe['deltaT'] / pd.Timedelta('1H'))
+    return dataframe
+
+
+if __name__ == '__main__':
+    data_dir = "dataGeneration/"
+    raw_data = pd.read_csv(f'{data_dir}adcl_data.csv', sep=';')
+
+    in_progress_data = filter_tanks_data(raw_data)
+    in_progress_data = calc_valves_from_tanks(in_progress_data)
+    in_progress_data = pumps_to_bool(in_progress_data)
+    in_progress_data = calc_level_variation(in_progress_data)
+    filtered_data = demands_to_volume(in_progress_data)
+    in_progress_data = filtered_data.to_csv(f'{data_dir}adcl_filtered_data.csv')
+    print('Finishing...')
